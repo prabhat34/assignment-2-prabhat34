6f1ec7036fe381670a934135f9c99130de80e631
prabhat34 <49153246+prabhat34@users.noreply.github.com>
2020-03-28 19:48:59 +0530

    problem 4 completed

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,5 +7,24 @@
 package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem4.myqueue.MyQueue;
+import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
+   public static void main(String[] args) {
+        MyBinarySearchTree mst=new MyBinarySearchTree();
+        mst.insert(40);
+        mst.insert(50);
+        mst.insert(40);
+        mst.insert(50);
+        mst.insert(70);
+        mst.insert(90);
+        mst.insert(10);
+        mst.insert(50);
+        mst.insert(60);
+        System.out.println("PreOrder Traversal : ");
+        mst.traversePreOrder();
+        MyQueue queue = new MyQueue();
+        queue.storePreOrderSuccessor(mytree);
+        queue.display();
+    }
 }
