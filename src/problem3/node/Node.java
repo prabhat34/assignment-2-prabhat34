b61acf85196e11870f65d776b795db1eec44cc6f
prabhat34 <49153246+prabhat34@users.noreply.github.com>
2020-03-28 19:43:51 +0530

    problem3 completed

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,6 +5,30 @@
  *  Time: 7:13 PM
  */
 package problem3.node;
-
+import problem5.student.Student;
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+  
 }
