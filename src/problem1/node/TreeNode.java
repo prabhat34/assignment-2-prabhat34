b9197612292d2049b57451e7fb743abb86c97e8c
prabhat34 <49153246+prabhat34@users.noreply.github.com>
2020-03-28 19:15:55 +0530

    problem1 completed

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,42 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: vaibhav
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
 package problem1.node;
 
-public class TreeNode {
+public class TreeNode {  private int data;
+    private TreeNode left;
+    private TreeNode right;
+    public TreeNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
 }
