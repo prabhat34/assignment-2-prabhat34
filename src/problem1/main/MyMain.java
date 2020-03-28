72d88e688cc1026abea43f6d01a0e9a0a4dda4cf
prabhat34 <49153246+prabhat34@users.noreply.github.com>
2020-03-28 19:02:13 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,21 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+    MyBinarySearchTree mst = new MyBinarySearchTree();
+            mst.insert(50);
+            mst.insert(30);
+            mst.insert(20);
+            mst.insert(40);
+            mst.insert(70);
+            mst.insert(60);
+            mst.insert(80);
+            mst.insert(90);
+            mst.insert(10);
+            System.out.println("PreOrder Traversal : ");
+            mst.traversePreOrder();
+            System.out.println("Left children Only : ");
+            //traverseLeft() methods prints all left children as well as returns the no of nodes not having left a child
+            System.out.println("No of nodes not having Left Subchild: " + mst.traverseLeft());
+
+        }
 }
