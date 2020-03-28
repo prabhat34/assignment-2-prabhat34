caa04334698f1aa629b920fc1715091e374e1bf2
prabhat34 <49153246+prabhat34@users.noreply.github.com>
2020-03-28 20:02:20 +0530

    problem 5 completed

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -6,5 +6,30 @@
  */
 package problem5.node;
 // to define node properties
+import problem5.student.Student;
 public class Node {
+   private int data;
+    private Node next;
+    private Student student;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
